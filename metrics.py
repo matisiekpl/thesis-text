2d29abef89e8a7460559ccd5e7e32a6910ea0c4e
Mateusz Woźniak <matisiek11@gmail.com>
2024-05-04 14:23:17 +0200

    new experiments

--- metrics.py
+++ metrics.py
@@ -18,37 +18,70 @@ with torch.no_grad():
         y_pred_val.extend(predicted.cpu().numpy())
 
 validation_loss_history.append(val_loss)
+x2.append(epoch+1)
 
 val_loss /= len(val_loader)
 val_accuracy = val_correct / len(val_dataset)
-validation_accuracy_history.append(val_accuracy)
 f1_val = f1_score(y_true_val, y_pred_val, average='weighted')
+validation_f1_history.append(f1_val)
 
-log(classification_report(y_true_val,  y_pred_val, target_names=dataset.classes))
+log(classification_report(y_true_val,
+                          y_pred_val, target_names=dataset.classes))
 log(f'Epoch {epoch+1}/{EPOCHS}, Validation Loss: {val_loss}, Val Accuracy: {val_accuracy}, F1: {f1_val}')
+
+plt.clf()
+plt.figure(figsize=(5, 7))
+plt.title('Wykres funkcji straty od epoki')
+plt.plot(x1, training_loss_history, label='Strata treningu')
+plt.plot(x2, validation_loss_history, label='Strata walidacji')
+plt.legend()
+plt.xticks(range(math.floor(min(x2)), math.ceil(max(x2))+1))
+plt.xlabel('Epoka')
+plt.ylabel('Strata')
+plt.savefig(f'{experiment_path}/loss.png', bbox_inches="tight")
+
+plt.clf()
+plt.figure(figsize=(5, 7))
+plt.title('Wykres F1 od epoki')
+plt.plot(x1, training_f1_history,
+         label='F1 dla danych treningowych')
+plt.plot(x2, validation_f1_history,
+         label='F1 dla danych walidacyjnych')
+plt.legend()
+plt.ylim(0, 1)
+plt.xticks(range(math.floor(min(x2)), math.ceil(max(x2))+1))
+plt.xlabel('Epoka')
+plt.ylabel('F1')
+plt.savefig(f'{experiment_path}/f1.png', bbox_inches="tight")
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 7))
+ax[0].plot(x1, training_loss_history, label='Strata treningu')
+ax[0].plot(x2, validation_loss_history, label='Strata walidacji')
+ax[0].legend()
+ax[0].set_title('Wykres funkcji straty od epoki')
+ax[0].set_xlabel('Epoka')
+ax[0].set_ylabel('Strata')
+ax[0].set_xticks(range(math.floor(min(x2)), math.ceil(max(x2))+1))
+ax[1].plot(x1, training_f1_history,
+           label='F1 dla danych treningowych')
+ax[1].plot(x2, validation_f1_history,
+           label='F1 dla danych walidacyjnych')
+ax[1].legend()
+ax[1].set_title('Wykres F1 od epoki')
+ax[1].set_xlabel('Epoka')
+ax[1].set_ylabel('F1')
+ax[1].set_ylim(0, 1)
+ax[1].set_xticks(range(math.floor(min(x2)), math.ceil(max(x2))+1))
+plt.savefig(f'{experiment_path}/combined.png', bbox_inches="tight")
 
 cf_matrix = confusion_matrix(y_true_val, y_pred_val)
 df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1)[:, None], index=[
     f'{names[i]} ({i})' for i in dataset.classes], columns=[f'{names[i]} ({i})' for i in dataset.classes])
-plt.cla()
+plt.clf()
 plt.figure(figsize=(12, 7))
 sn.heatmap(df_cm, annot=True)
 plt.title('Macierz pomylek')
-plt.savefig(f'{experiment_path}/confusion_matrix.png', bbox_inches="tight")
-
-plt.cla()
-plt.title('Wykres funkcji straty od epoki')
-plt.plot(training_loss_history, label='Strata treningu')
-plt.plot(validation_loss_history, label='Strata walidacji')
-plt.legend()
-plt.savefig(f'{experiment_path}/loss.png', bbox_inches="tight")
-
-plt.cla()
-plt.title('Wykres dokladnosci od epoki')
-plt.plot(training_accuracy_history, label='Dokladnosc dla danych treningowych')
-plt.plot(validation_accuracy_history, label='Dokladnosc dla danych walidacyjnych')
-plt.legend()
-plt.ylim(0, 1)
-plt.savefig(f'{experiment_path}/acc.png', bbox_inches="tight")
+plt.savefig(f'{experiment_path}/confusion_matrix.png',
+            bbox_inches="tight")
 
 torch.save(model.state_dict(), f'{experiment_path}/model.pth')
--- train.py
+++ train.py
@@ -1,5 +1,8 @@
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
+train_loader = DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE,
+                        shuffle=False, num_workers=4)
+
 
 model = models.efficientnet_b0(weights='DEFAULT')
 num_ftrs = model.classifier[1].in_features
@@ -14,30 +17,38 @@ model.to(device)
 training_loss_history = []
 validation_loss_history = []
 
-training_accuracy_history = []
-validation_accuracy_history = []
+training_f1_history = []
+validation_f1_history = []
 
 for epoch in range(epochs):
     model.train()
     running_loss = 0.0
     running_correct = 0
 
-    for inputs, labels in tqdm(train_loader):
-        inputs, labels = inputs.to(device), labels.to(device)
+    if not DRY:
+        y_true_train = []
+        y_pred_train = []
+        for inputs, labels in tqdm(train_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        outputs = model(inputs)
+            outputs = model(inputs)
 
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            _, predicted = torch.max(outputs, 1)
+            running_correct += (predicted == labels).sum().item()
 
-        _, predicted = torch.max(outputs, 1)
-        running_correct += (predicted == labels).sum().item()
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        running_loss += loss.item()
-    training_loss_history.append(running_loss)
-    running_accuracy = running_correct / len(train_dataset)
-    training_accuracy_history.append(running_accuracy)
+            y_true_train.extend(labels.cpu().numpy())
+            y_pred_train.extend(predicted.cpu().numpy())
+
+            running_loss += loss.item()
+        training_loss_history.append(running_loss)
+        running_accuracy = running_correct / len(train_dataset)
+        f1_train = f1_score(y_true_train, y_pred_train, average='weighted')
+        training_f1_history.append(f1_train)
+
     log(f'Epoch {epoch+1}/{EPOCHS}, Training Loss: {running_loss/len(train_loader)}')
